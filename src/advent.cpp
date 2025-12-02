49043722d0c9f260005bbf613c0fe585e2e798a4
dparker1 <dap2180@columbia.edu>
2025-12-01 22:29:31 -0500

    Day 1-1 Solved

---
+++ src/advent.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "advent.h"
+
+int day_1_1()
+{
+    std::ifstream ifile;
+    ifile.open("./data/input_1_1.txt");
+    
+    int pos = 50;
+    int final_count = 0;
+    std::string content;
+    while(std::getline(ifile, content))
+    {
+        size_t content_len = content.length();
+        int diff = std::stoi(content.substr(1, content_len - 1));
+        if(content[0] == 'L')
+        {
+            diff *= -1;
+        }
+        pos += diff;
+        while(pos < 0)
+        {
+            pos += 100;
+        }
+        while(pos > 99)
+        {
+            pos -= 100;
+        }
+        if(pos == 0)
+        {
+            final_count++;
+        }
+    }
+
+    return final_count;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "advent.h"
+
+int main()
+{
+    int answer = day_1_1();
+    std::cout << answer << std::endl;
+}
